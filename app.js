3bcb768ca3e4e751b7c3b049e7666dd600916799
Muhammad Raza <raza.dev145@gmail.com>
2025-02-07 20:04:41 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+alert("chapter 01")
+
+alert("Error! Please enter a valid password.")
+alert("Welcome to JS Land...\n Happy Coding!")
+alert("Welcome to JS Land...")
+alert("Happy Coding!\n Prevent this page from creating additional dialogs.");
+
+alert("chapter 02")
+
+
+
+alert("chapter 03")
